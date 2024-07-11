00075c59ec63503c5e3de694fd2866d31f56bda6
pranav <pranavmihra14022019@gmail.com>
2024-07-12 00:23:37 +0530

    user routes and controllers added

--- backend/conf/conf.js
+++ backend/conf/conf.js
@@ -4,7 +4,10 @@ const conf = {
   MONGO_URL: String(process.env.MONGO_URL),
   PORT:String(process.env.PORT),
   JWT_SECRET:String(process.env.JWT_SECRET),
-  NODE_ENV:String(process.env.NODE_ENV)
+  NODE_ENV:String(process.env.NODE_ENV),
+  CLOUDINARY_CLOUD_NAME:String(process.env.CLOUDINARY_CLOUD_NAME),
+  CLOUDINARY_API_KEY:String(process.env.CLOUDINARY_API_KEY),
+  CLOUDINARY_API_SECRET:String(process.env.CLOUDINARY_API_SECRET)
 };
 
 export default conf;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import conf from "./conf/conf.js";
-import authRouter from "./routes/auth.routes.js";
-import { connect } from "mongoose";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from './routes/user.routes.js'
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({ 
+  cloud_name: conf.CLOUDINARY_CLOUD_NAME, 
+  api_key: conf.CLOUDINARY_API_KEY, 
+  api_secret: conf.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
 
 const app = express();
 const PORT = conf.PORT || 8000;
@@ -11,7 +19,8 @@ const PORT = conf.PORT || 8000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRoutes);
+app.use('/api/users',userRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
