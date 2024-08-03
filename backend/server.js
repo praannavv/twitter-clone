76f0d7dd0a7cddecb89b7d1d52023bfda979e5dc
praannavv <143808706+praannavv@users.noreply.github.com>
2024-08-04 01:34:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
   cloud_name: conf.CLOUDINARY_CLOUD_NAME,
   api_key: conf.CLOUDINARY_API_KEY,
-  api_secret: conf.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+  api_secret: conf.CLOUDINARY_API_SECRET,
 });
 
 
